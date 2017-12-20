ccce98b9054b4369f9626acdfcb7c472b786d0ef
Julien Robert IUT <julienrob@gmail.com>
2017-12-20 09:16:43 +0100

    sphinx-easypython en tant que module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sphinx-easypython',
+    version='0.1',
+    author='Julien Robert',
+    author_email='julien.robert@univ-orleans.fr',
+    packages=find_packages(),
+    install_requires=[],
+    dependency_links=['git+https://gitlab.com/jrobert/easypython-testeur.git'],
+)
