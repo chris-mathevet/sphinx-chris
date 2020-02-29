20b32513bb2571427f7522af11b9d826913975be
Julien Robert <julienrob@gmail.com>
2020-02-29 13:51:15 +0100

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sphinx-easypython',
-    version='0.1',
+    version='0.2',
     author='Julien Robert',
     author_email='julien.robert@univ-orleans.fr',
     packages=find_packages(),
